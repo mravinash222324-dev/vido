cc1fe07a01461fb88e9b807704137ff052f486e2
ACER <DESKTOP-EHTMDQG>
2026-04-19 17:42:11 +0530

    Revert to public openrelay servers

--- src/app/api/turn/route.ts
+++ src/app/api/turn/route.ts
@@ -1,26 +1,23 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const domain = 'voughtinternational.metered.live';
-  const apiKey = 'jq22-ASlzgFdCu1XVytufGZWFAvmbrtghXU2aGdrVt9Akywv';
-  
   return NextResponse.json([
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:global.stun.twilio.com:3478' },
     {
-      urls: `turn:${domain}:80`,
-      username: 'voughtinternational',
-      credential: apiKey
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
     },
     {
-      urls: `turn:${domain}:443`,
-      username: 'voughtinternational',
-      credential: apiKey
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
     },
     {
-      urls: `turn:${domain}:443?transport=tcp`,
-      username: 'voughtinternational',
-      credential: apiKey
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
     }
   ]);
 }
