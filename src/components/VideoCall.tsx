d7ea8594815cd4e319465148d46969a58cb415c4
ACER <DESKTOP-EHTMDQG>
2026-04-19 17:12:58 +0530

    Add OpenRelay TURN servers

--- src/components/VideoCall.tsx
+++ src/components/VideoCall.tsx
@@ -55,7 +55,21 @@ export default function VideoCall() {
             iceServers: [
               { urls: 'stun:stun.l.google.com:19302' },
               { urls: 'stun:global.stun.twilio.com:3478' },
-              { urls: 'stun:stun.1und1.de:3478' }
+              {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+              },
+              {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+              },
+              {
+                urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+              }
             ]
           }
         });
